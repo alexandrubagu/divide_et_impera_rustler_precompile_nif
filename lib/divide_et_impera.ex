a8a1d7bf3f1e0a002ab1a62aa7bda0ba0e08fb49
Alexandru Bagu <alexandrubagu87@gmail.com>
2024-04-19 21:43:53 +0300

    implements divide et impera in elixir

--- lib/divide_et_impera.ex
+++ lib/divide_et_impera.ex
@@ -1,18 +1,22 @@
 defmodule DivideEtImpera do
   @moduledoc """
-  Documentation for `DivideEtImpera`.
+  Implements Divide et Impera algorithm
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DivideEtImpera.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @spec search([integer()], integer()) :: position :: integer() | :not_found
+  def search(list, target) when is_list(list) and is_number(target) do
+    do_search(list, target, 0, length(list) - 1)
   end
+
+  defp do_search(list, target, low, high) when low <= high do
+    middle = div(low + high, 2)
+
+    case Enum.at(list, middle) do
+      value when value == target -> middle
+      value when value > target -> do_search(list, target, low, middle - 1)
+      _ -> do_search(list, target, middle + 1, high)
+    end
+  end
+
+  defp do_search(_, _target, _low, _high), do: :not_found
 end
