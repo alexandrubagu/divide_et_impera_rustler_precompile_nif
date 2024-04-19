791839e684cd2feeb36f8bf14ef43c6871a8a108
Alexandru Bagu <alexandrubagu87@gmail.com>
2024-04-19 21:46:11 +0300

    adds benchee

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule DivideEtImpera.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.32"}
+      {:rustler, "~> 0.32"},
+      {:benchee, "~> 1.3"}
     ]
   end
 end
