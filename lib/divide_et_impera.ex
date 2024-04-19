d0b072e5d59060646e6d48b1c573ba3699c62b4f
Alexandru Bagu <alexandrubagu87@gmail.com>
2024-04-19 21:02:21 +0300

    initial commit

---
+++ lib/divide_et_impera.ex
@@ -0,0 +1,18 @@
+defmodule DivideEtImpera do
+  @moduledoc """
+  Documentation for `DivideEtImpera`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DivideEtImpera.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/divide_et_impera_test.exs
@@ -0,0 +1,8 @@
+defmodule DivideEtImperaTest do
+  use ExUnit.Case
+  doctest DivideEtImpera
+
+  test "greets the world" do
+    assert DivideEtImpera.hello() == :world
+  end
+end
